26a9bfeb030d993dd5aad92dd5bed43517cfe655
ltronneb <ltronneberg@gmail.com>
2020-08-28 10:48:16 +0200

    added more options for synergyscreen()

--- R/synergyscreen.R
+++ R/synergyscreen.R
@@ -12,6 +12,13 @@
 #' @param path string; path for saving output and plot for each individual experiment.
 #' @param parallel logical; if TRUE, parallel processing is utilized to run the screen
 #' @param max_cores integer; the maximum number of cores to utilize for the parallel processing
+#' @param type integer; the type of model used. Must be one of the following: 1 (Splines), 2 (GP with squared exponential kernel), 3 (GP with Matérn kernel) or 4 (GP with rational quadratic kernel).
+#' @param lower_asymptotes logical; if TRUE the model will estimate lower estimate of monotherapy curves.
+#' @param nu numeric; the nu parameter for the Matern kernel. Must be one of {`0.5`,`1.5`,`2.5`}
+#' @param method The method of estimation. Must be one of {`sampling`,`vb`} corresponding to full sampling, or variational Bayes.
+#' @param control list; passed on to the stan sampler, e.g. for setting adapt_delta.
+#' @param ... Arguments passed to `rstan::sampling` (e.g. iter, chains).  
+#'  
 #' 
 #' @details
 #' The elements of \strong{experiments} must themselves be lists with the following elements
@@ -37,7 +44,9 @@
 #'
 #' @import foreach doParallel parallel doSNOW
 
-synergyscreen = function(experiments, metric = c("rVUS_syn","rVUS_ant"), save_raw = T, save_plots = T, path = NULL, parallel=T, max_cores=NULL){
+synergyscreen = function(experiments, metric = c("rVUS_syn","rVUS_ant"), save_raw = T, save_plots = T, path = NULL, parallel=T, max_cores=NULL,
+                         type = 3, lower_asymptotes = T, nu = 1.5 , method = "sampling",
+                         control = list(), ...){
   # Check that path is not null, and if so, set it to work directory
   if (is.null(path)){
     path <- getwd()
@@ -72,14 +81,16 @@ synergyscreen = function(experiments, metric = c("rVUS_syn","rVUS_ant"), save_ra
                          # Do the fitting here
                          data <- ee
                          fit <- bayesynergy(data$y,data$x,drug_names = data$drug_names,
-                                            experiment_ID = data$experiment_ID)
+                                            experiment_ID = data$experiment_ID,type = type,
+                                            lower_asymptotes = lower_asymptotes, nu = nu,
+                                            method=method, control = control, ...)
                          # Saving raw
                          if (save_raw){
                            save(fit,file=paste0(path,paste(data$experiment_ID,data$drug_names[1],data$drug_names[2],"raw",sep="_")))
                          }
                          # Saving plots
                          if (save_plots){
-                           plot(fit,save_plot = T,path=path)
+                           suppressMessages(plot(fit,save_plot = T,path=path))
                          }
                          
                          # Posterior 
@@ -111,7 +122,7 @@ synergyscreen = function(experiments, metric = c("rVUS_syn","rVUS_ant"), save_ra
       }
       # Saving plots
       if (save_plots){
-        plot(fit,save_plot = T,path=path)
+        suppressMessages(plot(fit,save_plot = T,path=path))
       }
       
       # Posterior 
