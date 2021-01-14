92630418b6557fb52bae260c0bcff6d8eb26159b
ltronneb <ltronneberg@gmail.com>
2021-01-14 08:41:43 +0100

    updated data documentation

--- R/ONeil_A375.R
+++ R/ONeil_A375.R
@@ -2,7 +2,7 @@
 #'
 #' @description  An example dataset from a large drug combination screening experiment, on the A-375 human melanoma cell line.
 #' 
-#' @format A list containing two distinct dataset from the experiment. Each list element is itself a list of two elemnts: a matrix of viability scores, and a matrix of corresponding concentrations. The data was adapted from the data provided by the \code{synergyfinder} R package.
+#' @format A \link{synergyscreen} object, consisting of an initial run of 583 drug combinations on the A-375 human melanoma cell line. Contains a dataframe of summary statistics for 581 experiments, and a list containing two experiments that failed to process.
 #' 
 #' @references 
 #' O'Neil J, Benita Y, Feldman I, Chenard M, Roberts B, Liu Y, Li J, Kral A, Lejnine S, Loboda A, Arthur W, Cristescu R, Haines BB, Winter C, Zhang T, Bloecher A, Shumway SD. An Unbiased Oncology Compound Screen to Identify Novel Combination Strategies. Mol Cancer Ther. 2016 Jun;15(6):1155-62. doi: 10.1158/1535-7163.MCT-15-0843. Epub 2016 Mar 16. PMID: 26983881.
