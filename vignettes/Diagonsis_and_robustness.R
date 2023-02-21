5fd4b9b01f8c5acd746807b4eeda88b6afb3ceee
chizhang <andreachizhang@yahoo.com>
2023-02-21 17:36:25 +0100

    test usethis

--- vignettes/Diagonsis_and_robustness.R
+++
@@ -1,6 +0,0 @@
-## ----setup, include=FALSE-----------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE)
-
-## ---- eval = F----------------------------------------------------------------
-#  fit = bayesynergy(y, x, control = list(adapt_delta = 0.99))
-
--- vignettes/bayesynergy.R
+++ vignettes/bayesynergy.R
@@ -1,17 +1,3 @@
 ## ----setup, include=FALSE-----------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE)
 
-## ----load_example,cache=T-----------------------------------------------------
-library(bayesynergy)
-data("mathews_DLBCL")
-y = mathews_DLBCL[[1]][[1]]
-x = mathews_DLBCL[[1]][[2]]
-head(cbind(y,x))
-
-## ----regular_fit, cache = T, warning = FALSE----------------------------------
-fit = bayesynergy(y,x, drug_names = c("ibrutinib", "ispinesib"),
-                  units = c("nM","nM"),bayes_factor = T)
-
-## ----summary_fit,cache=T------------------------------------------------------
-summary(fit)
-
